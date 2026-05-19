ade2193436006699f4d83af58b5ebb1991f1701f
student <student@FJ-1025>
2026-05-19 11:42:26 +0900

    data.sqlを編集

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,5 @@ INSERT INTO genres(genre_name, is_income) VALUES('その他', FALSE);
 INSERT INTO users(user_name,email,password) VALUES('田中太郎', 'tanaka@aaa.com','himitu');
 INSERT INTO users(user_name,email,password) VALUES('鈴木一郎', 'suzuki@aaa.com','himitu');
 -- 項目テーブルデータ
-INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('食事代', 1, 3, 1200, '2026/05/01');
-INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('収入', 2, 1, 200000, '2026/05/15');
+INSERT INTO items(item_name, user_id, genre_id, price, add_date) VALUES('食事代', 1, 3, 1200, '2026/05/01');
+INSERT INTO items(item_name, user_id, genre_id, price, add_date) VALUES('収入', 2, 1, 200000, '2026/05/15');
