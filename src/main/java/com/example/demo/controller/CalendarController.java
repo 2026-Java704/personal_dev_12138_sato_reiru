b9694ab2dd26ad467c998b350160c05f6fd8e372
student <student@FJ-1025>
2026-05-27 17:29:15 +0900

    入力チェック、画像編集、削除のメソッドを追加

--- src/main/java/com/example/demo/controller/CalendarController.java
+++ src/main/java/com/example/demo/controller/CalendarController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.Genre;
 import com.example.demo.entity.Item;
@@ -27,6 +28,7 @@ import com.example.demo.repository.UserRepository;
 
 @Controller
 public class CalendarController {
+
 	private final Account account;
 	private final ItemRepository itemRepository;
 	private final GenreRepository genreRepository;
@@ -57,6 +59,7 @@ public class CalendarController {
 		if (Method.nonLogin(account)) {
 			return "redirect:/";
 		}
+		model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
 		model.addAttribute("genres", genreRepository.findAll());
 		return "add";
 	}
@@ -86,6 +89,7 @@ public class CalendarController {
 			errors.add("日付は必須です");
 		}
 		if (!errors.isEmpty()) {
+			model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
 			model.addAttribute("errors", errors);
 			model.addAttribute("genres", genreRepository.findAll());
 			return "add";
@@ -113,6 +117,7 @@ public class CalendarController {
 		if (Method.nonLogin(account)) {
 			return "redirect:/";
 		}
+		model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
 		model.addAttribute("year", year);
 		model.addAttribute("month", month);
 
@@ -125,7 +130,6 @@ public class CalendarController {
 			Map<Integer, String> images = new HashMap<>();
 			int size = 100;
 			for (Item item : items) {
-				System.out.println(item.getAddDate());
 				if (date.equals(item.getAddDate())) {
 					details.add(item);
 				}
@@ -198,6 +202,7 @@ public class CalendarController {
 		if (Method.nonLogin(account)) {
 			return "redirect:/";
 		}
+		model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
 		model.addAttribute("item", itemRepository.findById(id).get());
 		model.addAttribute("genres", genreRepository.findAll());
 		return "edit";
@@ -212,9 +217,30 @@ public class CalendarController {
 			@RequestParam(defaultValue = "") Integer genreId,
 			@RequestParam(defaultValue = "") Integer price,
 			@RequestParam(defaultValue = "") LocalDate addDate,
-			@RequestParam(defaultValue = "") String comment) {
+			@RequestParam(defaultValue = "") String comment,
+			Model model) {
 		if (Method.nonLogin(account)) {
 			return "redirect:/";
+		}
+		List<String> errors = new ArrayList<String>();
+		if (name.isEmpty()) {
+			errors.add("名前は必須です");
+		}
+		if (Objects.isNull(genreId)) {
+			errors.add("ジャンルは必須です");
+		}
+		if (Objects.isNull(price)) {
+			errors.add("金額は必須です");
+		}
+		if (Objects.isNull(addDate)) {
+			errors.add("日付は必須です");
+		}
+		if (!errors.isEmpty()) {
+			model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
+			model.addAttribute("errors", errors);
+			model.addAttribute("item", itemRepository.findById(id).get());
+			model.addAttribute("genres", genreRepository.findAll());
+			return "edit";
 		}
 		Item item = itemRepository.findById(id).get();
 		Genre genre = genreRepository.findById(genreId).get();
@@ -237,6 +263,67 @@ public class CalendarController {
 		itemRepository.deleteById(id);
 		return "redirect:/calendar/" + year + "/" + month;
 	}
-	//	@PostMapping("/calendar/{year}/{month}/{id}/image/edit")
 
+	@GetMapping("/calendar/{year}/{month}/{id}/image/edit")
+	public String editImage(
+			@PathVariable Integer year,
+			@PathVariable Integer month,
+			@PathVariable Integer id,
+			Model model) {
+		if (Method.nonLogin(account)) {
+			return "redirect:/";
+		}
+		model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
+
+		byte[] data = itemRepository.findById(id).get().getReceipt();
+		if (data != null) {
+			String image = Base64.getEncoder().encodeToString(data);
+			model.addAttribute("image", image);
+		}
+		return "image";
+	}
+
+	@PostMapping("/calendar/{year}/{month}/{id}/image/edit")
+	public String updateImage(
+			@PathVariable Integer year,
+			@PathVariable Integer month,
+			@PathVariable Integer id,
+			@RequestParam MultipartFile file,
+			@RequestParam String image,
+			Model model) {
+		if (Method.nonLogin(account)) {
+			return "redirect:/";
+		}
+		model.addAttribute("totalPrice", Method.calcTotalPriceMonth(itemRepository.findByUserId(account.getId())));
+
+		if (Objects.isNull(file) || file.isEmpty()) {
+			model.addAttribute("error", "画像が選択されていません");
+			if (!image.isEmpty()) {
+				model.addAttribute("image", image);
+			}
+			return "image";
+		}
+		try {
+			Item item = itemRepository.findById(id).get();
+			item.setReceipt(file.getBytes());
+			item.setFileType(file.getContentType());
+			itemRepository.save(item);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:/calendar/" + year + "/" + month;
+	}
+
+	@PostMapping("/calendar/{year}/{month}/{id}/image/delete")
+	public String imageDelete(
+			@PathVariable Integer year,
+			@PathVariable Integer month,
+			@PathVariable Integer id) {
+		System.out.println("image deleted.");
+		Item item = itemRepository.findById(id).get();
+		item.setReceipt(null);
+		item.setFileType(null);
+		itemRepository.save(item);
+		return "redirect:/calendar/" + year + "/" + month;
+	}
 }
